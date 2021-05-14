91000f0dc07e812bea9830b85d621cf0e8c7754a
Heshan Marasinghe <heshanmarasinghe@gmail.com>
2021-05-14 17:59:50 +0530

    Updated Authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,19 +5,21 @@ function authentication(req, res, next) {
     console.log("Authentication Middleware Executing..........");
     console.log("Request : " +req.method+ ": " +req.url);
     saveLog(req.url,req.method)
+    console.log("..........Done..........");
     next();
 }
 
+//Save the Request
 function saveLog(req, method) {
     try {
 
-      let newCategory = new log({
+      let newLog = new log({
         request: req,
         requestMethod: method,
         createdDate: Date.now()
       });
   
-      newCategory.save();
+      newLog.save();
     } catch (ex) {
       return res.status(500).send("Error :" + ex.Message);
     }
