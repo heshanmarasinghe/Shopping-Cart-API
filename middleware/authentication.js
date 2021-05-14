96d5710719e4a0513cac02aa05efbf712fbda46f
Heshan Marasinghe <heshanmarasinghe@gmail.com>
2021-05-14 17:05:45 +0530

    Added Request Log Middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,8 +1,26 @@
+const log = require("../models/log");
+
 //Initiating a Middleware
 function authentication(req, res, next) {
     console.log("Authentication Middleware Executing..........");
-    //console.log("Request : " +req.method+ ": " +req.url);
+    console.log("Request : " +req.method+ ": " +req.url);
+    saveLog(req.url,req.method)
     next();
 }
+
+function saveLog(req, method) {
+    try {
+
+      let newCategory = new log({
+        request: req,
+        requestMethod: method,
+        createdDate: Date.now()
+      });
+  
+      newCategory.save();
+    } catch (ex) {
+      return res.status(500).send("Error :" + ex.Message);
+    }
+  };
 
 module.exports = authentication
